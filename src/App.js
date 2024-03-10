d7ad7437af454e0b21decf5f5ddaf8c50973d9f0
fahadsmartlifekw <132986028+fahadsmartlifekw@users.noreply.github.com>
2024-03-10 14:16:41 +0300

    newotlwn

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ import Amanat from './pages/Amanat';
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
-  const [token, setToken] = useState(JSON.parse(sessionStorage.getItem('token')))
+  const [token, setToken] = useState(sessionStorage.getItem('token'))
 
   useEffect(() => {
-    
+
     console.log("this is the token: " + token);
     console.log("This is the authenticated state", authenticated);
     setAuthenticated(!!token);
