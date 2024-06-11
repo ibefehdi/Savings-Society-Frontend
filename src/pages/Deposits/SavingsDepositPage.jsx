998b9ff4825218c50282cd6f5b9a6695ff028ed9
fahadsmartlifekw <132986028+fahadsmartlifekw@users.noreply.github.com>
2024-06-11 18:38:16 +0300

    tofixedremoved

--- src/pages/Deposits/SavingsDepositPage.jsx
+++ src/pages/Deposits/SavingsDepositPage.jsx
@@ -139,22 +139,22 @@ const SavingsDepositPage = () => {
                 return `Block ${block}, Street ${street}, House ${house}, Avenue ${avenue}, City ${city}`;
             }
         },
-        {
-            field: 'initialInvestment',
-            headerName: t('initial_investment'),
-            flex: 1,
-            renderCell: (params) => {
-                return params.row.savings && params.row.savings.initialAmount.toFixed(3)
-            }
-        },
-        {
-            field: 'currentAmount',
-            headerName: t('current_amount'),
-            flex: 1,
-            renderCell: (params) => {
-                return params.row.savings && params.row.savings.currentAmount.toFixed(3)
-            }
-        },
+        // {
+        //     field: 'initialInvestment',
+        //     headerName: t('initial_investment'),
+        //     flex: 1,
+        //     renderCell: (params) => {
+        //         return params.row.savings && params.row.savings?.initialAmount?.toFixed(3)
+        //     }
+        // },
+        // {
+        //     field: 'currentAmount',
+        //     headerName: t('current_amount'),
+        //     flex: 1,
+        //     renderCell: (params) => {
+        //         return params.row.savings && params.row.savings?.currentAmount?.toFixed(3)
+        //     }
+        // },
         // {
         //     field: 'membershipStatus',
         //     headerName: t('membership_status'),
