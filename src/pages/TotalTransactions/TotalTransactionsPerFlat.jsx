4de7f02aa18177567006aa8fdfe709050abf7924
fahadsmartlifekw <132986028+fahadsmartlifekw@users.noreply.github.com>
2024-12-23 22:26:32 +0300

    Changed transactions

--- src/pages/TotalTransactions/TotalTransactionsPerFlat.jsx
+++ src/pages/TotalTransactions/TotalTransactionsPerFlat.jsx
@@ -10,7 +10,8 @@ import createCache from '@emotion/cache';
 import axiosInstance from '../../constants/axiosInstance';
 import BackButton from '../../components/BackButton';
 import { saveAs } from 'file-saver';
-import { Button } from '@mui/material';
+import { Button, MenuItem, TextField } from '@mui/material';
+import FilterListOutlinedIcon from '@mui/icons-material/FilterListOutlined';
 
 const TotalTransactionsPerFlat = () => {
     const cacheRtl = createCache({
@@ -24,19 +25,37 @@ const TotalTransactionsPerFlat = () => {
     const { t, i18n } = useTranslation();
     const [pageSize, setPageSize] = useState(10);
     const [incomes, setIncomes] = useState([]);
-    const [expenses, setExpenses] = useState([]);
     const isRtl = i18n.dir() === 'rtl';
+    const [buildings, setBuildings] = useState([]);
+    const [selectedBuilding, setSelectedBuilding] = useState('');
+    const [showFilters, setShowFilters] = useState(false);
+
+    const toggleFilters = () => {
+        setShowFilters(!showFilters);
+    };
+
+    // Fetch buildings for dropdown
+    useEffect(() => {
+        const fetchBuildings = async () => {
+            try {
+                const response = await axiosInstance.get('/buildingdropdown');
+                setBuildings(response?.data?.data);
+            } catch (error) {
+                console.error('Error fetching buildings:', error);
+            }
+        };
+        fetchBuildings();
+    }, []);
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [selectedBuilding]);
 
     const fetchData = async () => {
         try {
-            const incomeResponse = await axiosInstance.get('/incomes/flats');
-            const expenseResponse = await axiosInstance.get('/expenses/flats');
-            setIncomes(incomeResponse.data.incomes);
-            setExpenses(expenseResponse.data.expenses);
+            const buildingQuery = selectedBuilding ? `/${selectedBuilding}` : '';
+            const response = await axiosInstance.get(`/incomes/flats${buildingQuery}`);
+            setIncomes(response.data.flats);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -57,89 +76,124 @@ const TotalTransactionsPerFlat = () => {
             field: 'totalIncome',
             headerName: t('total_income'),
             flex: 1,
-        },
-        {
-            field: 'totalExpenses',
-            headerName: t('total_expenses'),
-            flex: 1,
-        },
-        {
-            field: 'profit',
-            headerName: t('profit'),
-            flex: 1,
-            valueGetter: (params) => Number(params.row.totalIncome) - Number(params.row.totalExpenses),
-        },
+        }
     ];
 
-    const rows = incomes.map((income) => {
-        const expense = expenses.find(
-            (expense) => expense.flatId === income.flatId
-        );
-        return {
-            id: income.flatId,
-            buildingName: income.buildingName,
-            flatNumber: income.flatNumber,
-            totalIncome: income.totalIncome,
-            totalExpenses: expense ? expense.totalExpenses : 0,
-        };
-    });
+    const rows = incomes.map((income) => ({
+        id: income.flatId,
+        buildingName: income.buildingName,
+        flatNumber: income.flatNumber,
+        totalIncome: income.totalIncome,
+    }));
 
     const totalIncome = incomes.reduce((sum, income) => sum + income.totalIncome, 0);
-    const totalExpenses = expenses.reduce((sum, expense) => sum + expense.totalExpenses, 0);
-    const totalProfit = totalIncome - totalExpenses;
 
-    // Add total income, total expenses, and total profit as the last row
+    // Add total row
     rows.push({
         id: 'total',
         buildingName: t('total'),
         flatNumber: '',
-        totalIncome,
-        totalExpenses,
-        profit: totalProfit,
+        totalIncome
     });
+
     const orderedColumns = isRtl ? [...columns].reverse() : columns;
+
     const downloadCSV = () => {
-        axiosInstance.get('/profit-report-flat/export', { responseType: 'blob' })
+        const buildingQuery = selectedBuilding ? `?buildingId=${selectedBuilding}` : '';
+        axiosInstance.get(`/income-report-flat/export${buildingQuery}`, { responseType: 'blob' })
             .then((response) => {
                 const blob = new Blob([response.data], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
-                saveAs(blob, "profit_report_by_flat.xlsx");
+                saveAs(blob, "income_report_by_flat.xlsx");
             })
             .catch(error => console.error('Download error!', error));
     };
+
     return (
         <CacheProvider value={isRtl ? cacheRtl : cacheLtr}>
-            <Box
+            <Button
+                onClick={toggleFilters}
+                variant="outlined"
                 sx={{
-                    width: '90%',
                     backgroundColor: '#FFF',
-                    margin: '2rem',
+                    marginLeft: '2rem',
+                    marginTop: '2rem',
+                    overflowX: 'auto',
+                    marginRight: isRtl ? '2rem' : 0
+                }}
+            >
+                <FilterListOutlinedIcon /> {t('filter')}
+            </Button>
+
+            {showFilters && (
+                <Box sx={{
+                    width: '90%',
+                    display: 'flex',
+                    gap: '1rem',
+                    backgroundColor: '#FFF',
+                    marginLeft: '2rem',
+                    marginTop: '2rem',
                     padding: '1rem',
                     borderRadius: '0.5rem',
                     overflowX: 'auto',
-                }}
-            >
-                <Typography
-                    variant="h3"
-                    component="h2"
-                    sx={{
-                        fontStyle: 'normal',
-                        fontWeight: 600,
-                        lineHeight: '1.875rem',
-                        flexGrow: 1,
-                        marginLeft: '1.2rem',
-                        marginBottom: '1rem',
-                    }}
-                >
-                    {t('profit_report_per_flat')}
-                </Typography>
-                <Button
-                    variant="contained"
-                    onClick={downloadCSV}
-                    sx={{ marginRight: '1rem' }}
-                >
-                    {t('export_xlsx')}
-                </Button>
-                <BackButton />
+                    marginRight: isRtl ? "2rem" : 0
+                }}>
+                    <TextField
+                        label={t('building')}
+                        variant="outlined"
+                        select
+                        value={selectedBuilding}
+                        onChange={(e) => setSelectedBuilding(e.target.value)}
+                        fullWidth
+                        autoComplete='off'
+                    >
+                        <MenuItem value="">
+                            {t('all_buildings')}
+                        </MenuItem>
+                        {buildings.map((building) => (
+                            <MenuItem key={building._id} value={building._id}>
+                                {building.name}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Box>
+            )}
+
+            <Box sx={{
+                width: '90%',
+                backgroundColor: '#FFF',
+                margin: '2rem',
+                padding: '1rem',
+                borderRadius: '0.5rem',
+                overflowX: 'auto',
+            }}>
+                <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    marginBottom: '1rem'
+                }}>
+                    <Typography
+                        variant="h3"
+                        component="h2"
+                        sx={{
+                            fontStyle: 'normal',
+                            fontWeight: 600,
+                            lineHeight: '1.875rem',
+                            flexGrow: 1,
+                            marginLeft: '1.2rem',
+                        }}
+                    >
+                        {t('income_report_per_flat')}
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        onClick={downloadCSV}
+                        sx={{ marginRight: '1rem' }}
+                    >
+                        {t('export_xlsx')}
+                    </Button>
+                    <BackButton />
+                </Box>
 
                 <DataGrid
                     rows={rows}
@@ -163,7 +217,6 @@ const TotalTransactionsPerFlat = () => {
                         border: 'none',
                         direction: isRtl ? 'rtl' : 'ltr',
                         textAlign: isRtl ? "right" : 'left',
-
                         width: '100%',
                         '& .MuiDataGrid-columnHeadersInner': {
                             border: 'none',
@@ -186,10 +239,9 @@ const TotalTransactionsPerFlat = () => {
                             fontSize: '0.875rem',
                         },
                         '& .MuiDataGrid-cell': {
-                            fontSize: '1rem', // Increase the font size of the data cells
-                            fontWeight: 'bold', // Make the data text bolder
+                            fontSize: '1rem',
+                            fontWeight: 'bold',
                         },
-
                     }}
                     disableRowSelectionOnClick
                 />
